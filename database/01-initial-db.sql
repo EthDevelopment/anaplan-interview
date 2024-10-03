e4baf0a575d37a2aaa86a17a6eb9726b6791609e
swaleh <ethereumdevelopment@gmail.com>
2024-10-03 13:27:31 +0100

    working api, smoke_tests, dockerised, last thing to do is make sure that time on automatically seeded data is updating correctly as currently it returns a null value

--- database/01-initial-db.sql
+++ database/01-initial-db.sql
@@ -1,9 +1,11 @@
-use definitions;
+USE definitions;
 
 CREATE TABLE IF NOT EXISTS `dashboards` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `createdAt` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `updatedAt` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+  `title` VARCHAR(255) NOT NULL,
+  `createdAt` TIMESTAMP NULL DEFAULT NULL,
+  `updatedAt` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
